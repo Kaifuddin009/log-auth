72849d0dcdb2bc63a45faa96f85bc67e5116e098
kaifuddin009 <kaifuddin01@email.com>
2025-08-12 23:11:02 +0530

    feat(client): initialize React client with Vite, TailwindCSS, and essential components
    - Added package.json for client dependencies and scripts.
    - Included public assets: background image and favicon.
    - Created main application structure with routing for Home, Login, Verify Email, and Reset Password pages.
    - Developed Header and Navbar components for navigation and branding.
    - Implemented AppContext for global state management.
    - Designed email templates for verification and password reset.
    - Established basic styles with TailwindCSS and custom CSS.
    - Removed outdated user routes from server.

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import VerifyEmail from './pages/VerifyEmail'
+import ResetPassword from './pages/ResetPassword';
+import {ToastContainer} from 'react-toastify';
+
+
+const App = () => {
+  return (
+    <div>
+      <ToastContainer/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/verifyemail' element={<VerifyEmail/>}/>
+        <Route path='/reset-password' element={<ResetPassword/>}/>
+      </Routes>
+      
+    </div>
+  )
+}
+
+export default App
